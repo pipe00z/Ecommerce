3214f6dad66aa52a8555f339892c97d64cf2db3b
pipe00z <pipe00z@hotmail.com>
2021-03-27 15:31:09 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/Footer.css'
+
+const Footer = () => {
+    return ( 
+        <div className="Footer">
+            <p className="Footer-title">Shop</p>
+            <p className="Footer-copy">Derechos reservados</p>
+        </div>
+     );
+}
+ 
+export default Footer;
